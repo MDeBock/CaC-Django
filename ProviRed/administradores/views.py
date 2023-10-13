4e9b0d6a0a481daea47ba992f958f3dd9ee91c96
joakosin861 <joakorc6@gmail.com>
2023-10-12 23:21:08 -0300

    Actualizado renders cambio de nombre templates

--- ProviRed/administradores/views.py
+++ ProviRed/administradores/views.py
@@ -6,8 +6,8 @@ def usuarios(requests):
     users = {
         'joaqui': {'rol': 'admin'}, 'pablo': {'rol': 'proveedor'}, 'mdeboc': {'rol': 'admin'}
     }
-    return render(requests, 'administracion/home.html', {'users': users})
+    return render(requests, 'administradores/home.html', {'users': users})
 
 
 def facturas(requests, id_factura):
-    return render(requests, 'administracion/registro_facturas.html')
+    return render(requests, 'administradores/registro_facturas.html')
